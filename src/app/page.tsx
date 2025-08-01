0da9bc7c0c2423ce549d20800a9ddc33aac74f5f
jmjubaer <jmjubaer3927@gmail.com>
2025-08-01 16:27:53 +0600

    add simple banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from '@/components/pages/home/Banner';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
-      
+        <Banner/>
     </div>
   );
 };
--- src/components/pages/home/Banner.tsx
+++ src/components/pages/home/Banner.tsx
@@ -1,9 +1,17 @@
-
+import { FaSearch } from "react-icons/fa";
 
 const Banner = () => {
     return (
-        <div>
-        
+        <div className='gradient_bg py-20'>
+            <div className='container text-white text-center'>
+                <h1 className='md:text-7xl sm:text-5xl text-3xl font-semibold '>
+                    Welcome to E-Shop
+                </h1>
+                <h2 className='md:text-2xl sm:text-xl text-sm mt-3'>
+                    Discover premium products at unbeatable prices
+                </h2>
+    
+            </div>
         </div>
     );
 };
